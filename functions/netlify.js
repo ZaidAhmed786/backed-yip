1e515dcbd9961e9babf1384a1be9755a12a2ed7d
saad8212 <saad.shabbir1435@gmail.com>
2024-05-21 15:41:18 -0700

    deploying to netlify

--- functions/netlify.js
+++ functions/netlify.js
@@ -1,40 +1,40 @@
 const express = require("express");
-require("./db");
-const app = express();
+require("../db.js");
 const path = require("path");
 const cors = require("cors");
-const { specs } = require("./config.js");
+const { specs } = require("../config.js");
 const swaggerUI = require("swagger-ui-express");
-const routes = require('./routes/routes'); 
+const routes = require('../routes/routes'); 
+const serverless = require("serverless-http");
+
+const app = express();
 const PORT = process.env.PORT || 9000;
+
 app.set("view engine", "ejs");
-const serverless = require("serverless-http")
+
 /************** Middlewares ****************/
 app.use(express.static(path.resolve('./public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "*");
-	next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
 });
 let corsOptions = {
     origin: '*',
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
+}
 app.use(cors(corsOptions));
 
-
 /************** Routes ****************/
-app.use('/' ,routes); /*** Application Route ***/ 
+app.use('/', routes); /*** Application Route ***/ 
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs)); /*** Documentation Route ***/
 
-
 /*** Listen to Port ***/
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
   console.log("listening on port http://localhost:" + PORT);
 });
+
 module.exports.handler = serverless(app);
-
-
